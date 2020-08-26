83320c0bf25e5dcf3805e3310a6ab4d1e2c9d88e
firmy90 <66943299+firmy90@users.noreply.github.com>
2020-08-26 18:16:35 +0200

    Delete coutries.sql

--- coutries.sql
+++
@@ -1,3 +0,0 @@
-insert into countries values (1,'Poland'),(2, 'Belarus'), (3, 'Ukraine'), (4,'Germany'), (5, 'Lithuania'),
-                             (6,'Slovakia'),(7, 'Czechia'), (8, 'Sweden'), (9,'Kazakhstan'), (10, 'United States'),
-                             (11,'United Kingdom'),(12, 'Austria'), (13, 'Other'), (14,'Russia');
